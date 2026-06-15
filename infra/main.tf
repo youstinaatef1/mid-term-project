db9289be32821c3ba24e130966553dc7316daec2
youstinaatef1 <youstinaatef748@gmail.com>
2026-06-15 14:46:37 +0300

    Done

--- infra/main.tf
+++ infra/main.tf
@@ -1,88 +1,92 @@
 terraform {
- required_version = ">= 1.5.0"
+  required_version = ">= 1.5.0"
 
- required_providers {
- aws = {
- source = "hashicorp/aws"
- version = "~> 5.0"
- }
- }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 provider "aws" {
- region = var.aws_region
+  region = var.aws_region
 }
 
+# 1. Network Setup (VPC)
 module "vpc" {
- source = "terraform-aws-modules/vpc/aws"
- version = "~> 5.0"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
 
- name = var.vpc_name
- cidr = var.vpc_cidr
- azs = var.azs
- private_subnets = var.private_subnets
- public_subnets = var.public_subnets
- enable_nat_gateway = var.enable_nat_gateway
- enable_vpn_gateway = var.enable_vpn_gateway
+  name               = var.vpc_name
+  cidr               = var.vpc_cidr
+  azs                = var.azs
+  private_subnets    = var.private_subnets
+  public_subnets     = var.public_subnets
+  enable_nat_gateway = var.enable_nat_gateway
+  enable_vpn_gateway = var.enable_vpn_gateway
 
- tags = {
- Terraform = "true"
- Environment = var.environment
- }
+  tags = {
+    Terraform   = "true"
+    Environment = var.environment
+  }
 }
 
+# 2. Data Source for AMI
 data "aws_ami" "amazon_linux" {
- most_recent = true
- owners = ["amazon"]
+  most_recent = true
+  owners      = ["amazon"]
 
- filter {
- name = "name"
- values = ["amzn2-ami-hvm-*-x86_64-gp2"]
- }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
 }
 
+# 3. Security Group
 resource "aws_security_group" "web" {
- name = "web-sg"
- description = "Allow HTTP and SSH access"
- vpc_id = module.vpc.vpc_id
+  name        = "web-sg"
+  description = "Allow HTTP and SSH access"
+  vpc_id      = module.vpc.vpc_id
 
- ingress {
- description = "HTTP"
- from_port = 80
- to_port = 80
- protocol = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
- }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
- ingress {
- description = "SSH"
- from_port = 22
- to_port = 22
- protocol = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
- }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
- egress {
- from_port = 0
- to_port = 0
- protocol = "-1"
- cidr_blocks = ["0.0.0.0/0"]
- }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
- tags = {
- Name = "web-sg"
- Environment = var.environment
- }
+  tags = {
+    Name        = "web-sg"
+    Environment = var.environment
+  }
 }
 
+# 4. EC2 Instance
 module "ec2" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 4.0"
+  version = "~> 5.0" 
 
   name                        = "web-server"
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = var.instance_type
-  subnet_ids                  = [module.vpc.public_subnets[0]]
+  subnet_id                   = module.vpc.public_subnets[0] 
   vpc_security_group_ids      = [aws_security_group.web.id]
   associate_public_ip_address = true
 
